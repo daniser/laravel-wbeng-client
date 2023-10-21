3ca8eb110644ecabcbccca13dc445c0f7b521d50
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2023-10-22 00:41:42 +0200

    Search flights CLI

--- src/Console/SearchCommand.php
+++ src/Console/SearchCommand.php
@@ -6,8 +6,12 @@ namespace TTBooking\WBEngine\Console;
 
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Helper\TableCell;
+use Symfony\Component\Console\Helper\TableCellStyle;
 use TTBooking\WBEngine\Contracts\ClientFactory;
-use function TTBooking\WBEngine\fly;
+use TTBooking\WBEngine\DTO\Air\SearchFlights\Response;
+use function Laravel\Prompts\{info, note, spin, table, text};
+use function TTBooking\WBEngine\{data_get, fly};
 
 #[AsCommand(
     name: 'wbeng:search',
@@ -21,9 +25,9 @@ class SearchCommand extends Command
      * @var string
      */
     protected $signature = 'wbeng:search
-        {from : Origin location code}
-        {to : Destination location code}
-        {date : Flight date}
+        {from? : Origin location code}
+        {to? : Destination location code}
+        {date? : Flight date}
         {--connection= : Using connection}';
 
     /**
@@ -38,13 +42,58 @@ class SearchCommand extends Command
      */
     public function handle(ClientFactory $clientFactory): void
     {
-        $clientFactory->connection($this->option('connection'))
-            ->searchFlights(fly()
-                ->from($this->argument('from'))
-                ->to($this->argument('to'))
-                ->at($this->argument('date'))
-            );
+        $origin = $this->argument('from') ?? text(
+            label: 'From',
+            placeholder: 'MOW',
+            required: true,
+            hint: 'Departure location code',
+        );
 
-        $this->info('Search successfully finished.');
+        $destination = $this->argument('to') ?? text(
+            label: 'To',
+            placeholder: 'LED',
+            required: true,
+            hint: 'Arrival location code',
+        );
+
+        $date = $this->argument('date') ?? text(
+            label: 'Date',
+            placeholder: date('Y-m-d'),
+            required: true,
+            hint: 'Date of departure',
+        );
+
+        $result = spin(fn (): Response => $clientFactory->connection($this->option('connection'))->searchFlights(
+            fly()->from($origin)->to($destination)->at($date)
+        ), 'Searching flights...');
+
+        note(sprintf(
+            "<question>%s</question>\t\t%s\t\t<comment>%s</comment>\t\t<info>%s</info>",
+            'WBENG '.$result->context->version,
+            $result->context->environment,
+            $result->context->profile,
+            implode(',', $result->context->provider),
+        ));
+
+        $rows = [];
+        foreach ($result->flightGroups as $flightGroup) {
+            $rows[] = [
+                data_get($flightGroup, 'itineraries.^.flights.^.segments.^.dateBegin')->format('H:i'),
+                data_get($flightGroup, 'itineraries.$.flights.$.segments.$.dateEnd')->format('H:i'),
+                $flightGroup->carrier->code.' '.$flightGroup->provider.' '.$flightGroup->gds,
+                data_get($flightGroup, 'itineraries.^.flights.^.segments.^.locationBegin.code').'-'.
+                data_get($flightGroup, 'itineraries.$.flights.$.segments.$.locationEnd.code'),
+                data_get($flightGroup, 'itineraries.0.flights.0.segments.0.carrier.code').'-'.
+                data_get($flightGroup, 'itineraries.0.flights.0.segments.0.flightNumber'),
+                new TableCell(
+                    (string) $flightGroup->fares->fareTotal,
+                    ['style' => new TableCellStyle(['align' => 'right'])]
+                ),
+            ];
+        }
+
+        table(['Departure', 'Arrival', 'Carrier/GDS', 'Route', 'Flight', 'Fare total'], $rows);
+
+        info('Search successfully finished.');
     }
 }
--- src/helpers.php
+++ src/helpers.php
@@ -4,9 +4,68 @@ declare(strict_types=1);
 
 namespace TTBooking\WBEngine;
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 use TTBooking\WBEngine\Facades\WBEngine;
 
 function wbeng(string $connection = null): ClientInterface
 {
     return WBEngine::connection($connection);
+}
+
+/**
+ * Get an item from an array or object using "dot" notation.
+ *
+ * @param  mixed  $target
+ * @param  string|array<string|int|null>|int|null  $key
+ * @param  mixed  $default
+ * @return mixed
+ */
+function data_get(mixed $target, string|array|int|null $key, mixed $default = null): mixed
+{
+    if (is_null($key)) {
+        return $target;
+    }
+
+    $key = is_array($key) ? $key : explode('.', (string) $key);
+
+    foreach ($key as $i => $segment) {
+        unset($key[$i]);
+
+        if (is_null($segment)) {
+            return $target;
+        }
+
+        if ($segment === '*') {
+            if ($target instanceof Collection) {
+                $target = $target->all();
+            } elseif (! is_iterable($target)) {
+                return value($default);
+            }
+
+            $result = [];
+
+            foreach ($target as $item) {
+                $result[] = data_get($item, $key);
+            }
+
+            return in_array('*', $key) ? Arr::collapse($result) : $result;
+        }
+
+        $segment = match ($segment) {
+            '^' => array_key_first($target),
+            '$' => array_key_last($target),
+            default => $segment,
+        };
+
+        if (Arr::accessible($target) && Arr::exists($target, $segment)) {
+            $target = $target[$segment];
+        } elseif (is_object($target) && isset($target->{$segment})) {
+            $target = $target->{$segment};
+        } else {
+            return value($default);
+        }
+    }
+
+    return $target;
 }
