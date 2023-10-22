a7c2611450c7a45f1771f4caad3b22eaa0084de8
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2023-10-22 13:47:40 +0200

    Search CLI: default presentation switched to "select" on supported systems

--- src/Console/SearchCommand.php
+++ src/Console/SearchCommand.php
@@ -12,7 +12,7 @@ use Symfony\Component\Console\Helper\TableCellStyle;
 use TTBooking\WBEngine\Contracts\ClientFactory;
 use TTBooking\WBEngine\DTO\Air\SearchFlights\Response;
 
-use function Laravel\Prompts\{info, note, spin, table, text};
+use function Laravel\Prompts\{info, note, select, spin, table, text, warning};
 use function TTBooking\WBEngine\{data_get, fly};
 
 #[AsCommand(
@@ -30,7 +30,8 @@ class SearchCommand extends Command
         {from? : Origin location code}
         {to? : Destination location code}
         {date? : Flight date}
-        {--connection= : Using connection}';
+        {--connection= : Using connection}
+        {--table : Present results in table form}';
 
     /**
      * The console command description.
@@ -42,7 +43,7 @@ class SearchCommand extends Command
     /**
      * Execute the console command.
      */
-    public function handle(ClientFactory $clientFactory): void
+    public function handle(ClientFactory $clientFactory): int
     {
         $origin = $this->argument('from') ?? text(
             label: 'From',
@@ -77,10 +78,17 @@ class SearchCommand extends Command
             implode(',', $result->context->provider),
         ));
 
+        if (! $result->flightGroups) {
+            warning('No flights found.');
+
+            return static::FAILURE;
+        }
+
         $rows = [];
-        $flightGroups = Arr::sort($result->flightGroups, 'fares.fareTotal');
-        foreach ($flightGroups as $flightGroup) {
-            $rows[] = [
+        $table = windows_os() || $this->option('table');
+        $flightGroups = array_values(Arr::sort($result->flightGroups, 'fares.fareTotal'));
+        foreach ($flightGroups as $id => $flightGroup) {
+            $row = [
                 data_get($flightGroup, 'itineraries.^.flights.^.segments.^.dateBegin')->format('H:i'),
                 data_get($flightGroup, 'itineraries.$.flights.$.segments.$.dateEnd')->format('H:i'),
                 $flightGroup->carrier->code.' '.$flightGroup->provider.' '.$flightGroup->gds,
@@ -88,15 +96,47 @@ class SearchCommand extends Command
                 data_get($flightGroup, 'itineraries.$.flights.$.segments.$.locationEnd.code'),
                 data_get($flightGroup, 'itineraries.0.flights.0.segments.0.carrier.code').'-'.
                 data_get($flightGroup, 'itineraries.0.flights.0.segments.0.flightNumber'),
-                new TableCell(
-                    (string) $flightGroup->fares->fareTotal,
-                    ['style' => new TableCellStyle(['align' => 'right'])]
-                ),
+                $table ? self::rcell($flightGroup->fares->fareTotal) : $flightGroup->fares->fareTotal,
             ];
+
+            if ($table) {
+                $rows[] = [self::rcell($id + 1), ...$row];
+            } else {
+                $rows[$id + 1] = sprintf('%s  %s  %-11s  %s  %-7s  %7d', ...$row);
+            }
         }
 
-        table(['Departure', 'Arrival', 'Carrier/GDS', 'Route', 'Flight', 'Fare total'], $rows);
+        if ($table) {
+            table(['#', 'Departure', 'Arrival', 'Carrier/GDS', 'Route', 'Flight', 'Fare total'], $rows);
+
+            $flightId = (int) text(
+                label: 'Enter flight #',
+                required: true,
+                validate: static fn (string $value) => filter_var($value, FILTER_VALIDATE_INT, ['options' => [
+                    'min_range' => 1,
+                    'max_range' => count($rows),
+                ]]) === false ? 'Please enter valid flight #' : null,
+                hint: 'Choose flight to inspect its details',
+            );
+        } else {
+            $flightId = select(
+                label: 'Select flight',
+                options: $rows,
+                scroll: 25,
+                hint: 'Choose flight to inspect its details',
+            );
+        }
+
+        info(gettype($flightId));
+        info((string) $flightId);
 
         info('Search successfully finished.');
+
+        return static::SUCCESS;
+    }
+
+    private static function rcell(string|int $value): TableCell
+    {
+        return new TableCell((string) $value, ['style' => new TableCellStyle(['align' => 'right'])]);
     }
 }
