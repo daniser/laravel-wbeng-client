a7e7882defe6e146ccfb19bff16f278fda9a0991
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2023-12-05 17:12:26 +0200

    Database state storage

--- src/Stores/DatabaseStorage.php
+++ src/Stores/DatabaseStorage.php
@@ -4,27 +4,60 @@ declare(strict_types=1);
 
 namespace TTBooking\WBEngine\Stores;
 
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Database\ConnectionInterface;
+use Illuminate\Support\Str;
 use TTBooking\WBEngine\Contracts\StateStorage;
+use TTBooking\WBEngine\Exceptions\StateNotFoundException;
+use TTBooking\WBEngine\ResultInterface;
+use TTBooking\WBEngine\SerializerInterface;
 use TTBooking\WBEngine\StorableState;
 
 class DatabaseStorage implements StateStorage
 {
-    public function __construct(protected string $table = 'wbeng_state')
-    {
+    public function __construct(
+        protected Container $container,
+        protected SerializerInterface $serializer,
+        protected ConnectionInterface $connection,
+        protected string $table = 'wbeng_state',
+    ) {
     }
 
     public function has(string $id): bool
     {
-        // TODO: Implement has() method.
+        return $this->connection->table($this->table)->where('uuid', $id)->exists();
     }
 
     public function get(string $id): StorableState
     {
-        // TODO: Implement get() method.
+        $record = $this->connection->table($this->table)->where('uuid', $id)->first();
+
+        if (! $record) {
+            throw new StateNotFoundException("State [$id] not found");
+        }
+
+        $record = (array) $record;
+
+        /** @var StorableState<ResultInterface> $state */
+        $state = $this->container->make(StorableState::class);
+
+        return $state
+            ->id($record['uuid'])
+            ->baseUri($record['base_uri'])
+            ->query($record['query'])
+            ->result($record['result']);
     }
 
     public function put(StorableState $state): StorableState
     {
-        // TODO: Implement put() method.
+        $this->connection->table($this->table)->insert([
+            'uuid' => $id = $state->id ?? (string) Str::orderedUuid(),
+            'base_uri' => $state->baseUri,
+            'endpoint' => $state->query::getEndpoint(),
+            'query' => $this->serializer->serialize($state->query),
+            'result' => $this->serializer->serialize($state->result),
+        ]);
+
+        return $state->id($id);
     }
 }
